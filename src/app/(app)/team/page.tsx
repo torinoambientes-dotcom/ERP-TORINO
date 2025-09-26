816fb2f586c185bee1c40c1cc7b9f36772f74f7a
Torino Ambientes <torinoambientes@gmail.com>
2025-09-26 10:34:16 +0000

    Os Marceneiros cadastrados na equipe devem aparacer somente na etapa pré

--- src/app/(app)/team/page.tsx
+++ src/app/(app)/team/page.tsx
@@ -82,6 +82,7 @@ export default function TeamPage() {
   };
   
     const getInitials = (name: string) => {
+      if (!name) return '';
       const names = name.split(' ');
       if (names.length > 1) {
         return `${names[0][0]}${names[names.length - 1][0]}`.toUpperCase();
