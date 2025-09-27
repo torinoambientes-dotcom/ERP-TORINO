7bef85c5c93015ef3efa636bf8ffdd250bb549a2
Torino Ambientes <torinoambientes@gmail.com>
2025-09-27 13:29:16 +0000

    após a alteração do nome, o sistema ficou diferente, perdi o q ja havia

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "torinoerp",
+  "projectId": "studio-9055172080-bc4a0",
   "appId": "1:1004857366081:web:9167dae3a942c4b58198c0",
   "apiKey": "AIzaSyC3vp8TSphudgNHUErh0ksNpMdvgvVDM2s",
-  "authDomain": "torinoerp.firebaseapp.com",
+  "authDomain": "studio-9055172080-bc4a0.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "1004857366081"
 };
